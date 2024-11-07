3a171517f33a25def2215519e60111f192657e4a
Corey Knight <sc21ck@leeds.ac.uk>
2024-11-07 16:50:56 +0000

    example layout for test

--- main.py
+++ main.py
@@ -3,4 +3,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-st.write("Hello World!")
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns([3, 1])
+
+# sample data
+data = np.random.randn(10, 1)
+
+col1.subheader("Example Chart location")
+
+# change this to pyplot implementation
+col1.line_chart(data)
+
+col2.subheader("Which school had the lowest number absences in February?")
+col2.write(['School 5', 'School 2', 'School 8', 'School 6'])
